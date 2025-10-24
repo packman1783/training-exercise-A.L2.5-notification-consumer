b9ce7d8aca50f7351da708f8f575a641c29b3fdc
Denis Saplitsyn <packman17@mail.ru>
2025-10-24 20:51:22 +0300

    add swagger reporter

---
+++ src/main/java/com/example/spring_kafka/config/OpenApiExporter.java
@@ -0,0 +1,33 @@
+package com.example.spring_kafka.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.swagger.v3.oas.models.OpenAPI;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.nio.file.Path;
+
+@Configuration
+public class OpenApiExporter {
+    @Bean
+    public ApplicationRunner exportOpenApi(OpenAPI openAPI) {
+        return args -> {
+            try {
+                Path outputPath = Path.of("src/main/resources/static/openapi.json");
+
+                ObjectMapper jsonMapper = new ObjectMapper();
+                jsonMapper.findAndRegisterModules();
+
+                jsonMapper.writerWithDefaultPrettyPrinter()
+                        .writeValue(outputPath.toFile(), openAPI);
+
+                System.out.println("OpenAPI specification (JSON) exported to: " + outputPath.toAbsolutePath());
+            } catch (Exception e) {
+                System.err.println("Error when exporting OpenAPI specification: " + e.getMessage());
+            }
+        };
+    }
+}
