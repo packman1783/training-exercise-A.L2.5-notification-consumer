ff6ab6e0bb040525bc88fa1d42272a4493f3a2fb
Denis Saplitsyn <packman17@mail.ru>
2025-10-29 19:39:46 +0300

    remove unused import

--- src/main/java/com/example/spring_kafka/controller/NotificationController.java
+++ src/main/java/com/example/spring_kafka/controller/NotificationController.java
@@ -9,7 +9,6 @@ import jakarta.validation.Valid;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
